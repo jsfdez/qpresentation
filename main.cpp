ac46f6f3144b9025b1c40eae664a54d0ef0c1bd3
Jesus Fernandez <jsfdez@gmail.com>
2024-12-04 21:00:50 +0100

    presentation finished

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
     QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGLRhi);
     QtWebEngineQuick::initialize();
     QGuiApplication app(argc, argv);
-    app.setApplicationName("Vida del Programador");
+    app.setApplicationName("");
     app.setOrganizationName("jsfdez");
 
     QQmlApplicationEngine engine;
